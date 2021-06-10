5c9689a5234d0f2e67286ff0ee5393be61fa0482
Kabira Singh <yogesh.baghel86@gmail.com>
2021-06-10 18:19:19 +0530

    DFS adjacency matrix

---
+++ Graph/DFS adjacency matrix.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+printDFS(int** edges,int sv,int n,bool* visited){
+	cout<<sv<<endl;
+	visited[sv]=true;
+	for(int i=0;i<n;i++){
+		if(!visited[i] && edges[sv][i]==1){
+			printDFS(edges,i,n,visited);	
+		}
+	}
+}
+
+int main(){
+	int n;
+	int e;
+	cin>>n>>e;
+	int** edges=new int*[n];
+	for(int i=0;i<n;i++){
+		edges[i]= new int[n];
+		for(int j=0;j<n;j++){
+			edges[i][j]=0;
+		}
+	}
+	
+	for(int i=0;i<e;i++){
+		int f,s;
+		cin>>f>>s;
+		edges[f][s]=1;
+		edges[s][f]=1;
+	}
+	
+	bool visited[n];
+	
+	for(int i=0;i<n;i++){
+		visited[i]=false;
+	}
+	
+	printDFS(edges,0,n,visited);
+}
