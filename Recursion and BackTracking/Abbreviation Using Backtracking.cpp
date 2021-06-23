24a80d4bd5e4597ea199237534d1f0321d45c76a
Kabira Singh <yogesh.baghel86@gmail.com>
2021-06-23 13:59:49 +0530

    Abbreviation Using Backtracking

---
+++ Recursion and BackTracking/Abbreviation Using Backtracking.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void solution(string str,string asf,int count,int pos){
+	
+	if(pos==str.length()){
+		if(count==0){
+			cout<<asf<<endl;	
+		}
+		else{
+			cout<<asf+to_string(count)<<endl;
+		}
+		return;
+	}
+	
+	if(count>0)
+		solution(str,asf+to_string(count)+str[pos],0,pos+1);
+	else
+		solution(str,asf+str[pos],0,pos+1);
+	solution(str,asf,count+1,pos+1);
+}
+
+int main(){
+	string s;
+	cin>>s;
+	solution(s,"",0,0);
+}
